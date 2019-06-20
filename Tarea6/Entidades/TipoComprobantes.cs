ef37ac776b336b840455d2f6ee0f253d68dd8325
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-20 10:35:47 -0400

    Agregando la entidad TIpoComprobantes

---
+++ Tarea6/Entidades/TipoComprobantes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+    class TipoComprobantes
+    {
+        public int IdComprobante { get; set; }
+        public string NombreComprobante { get; set; }
+
+        public TipoComprobantes()
+        {
+            IdComprobante = 0;
+            NombreComprobante = string.Empty;
+        }
+    }
+}
