56d6fcb151707e5c39da891682f349bdda4d1ed3
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-20 09:48:01 -0400

    Agregando la entidad marca

---
+++ Tarea6/Entidades/Marcas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+    class Marcas
+    {
+        public int IdMarca { get; set; }
+        public string NombreMarca { get; set; }
+
+        public Marcas()
+        {
+            IdMarca = 0;
+            NombreMarca = string.Empty;
+        }
+    }
+}
