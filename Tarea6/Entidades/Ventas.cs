4cf7176a21e3a22bd4a8611527be8e923c350a52
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-19 22:58:52 -0400

    Agregando la entidad DetallesVentas

--- Tarea6/Entidades/Ventas.cs
+++ Tarea6/Entidades/Ventas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Tarea6.Entidades
 {
     class Ventas
     {
+        [Key]
         public int IdVenta { get; set; }
         public int IdUsuario { get; set; }
         public int IdCliente { get; set; }
