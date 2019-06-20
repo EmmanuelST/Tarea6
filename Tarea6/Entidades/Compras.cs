12ec8b41357a954dfbf0423fb12cce6502899d65
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-20 09:55:43 -0400

    Agregando la entiidad Compras

---
+++ Tarea6/Entidades/Compras.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+    class Compras
+    {
+        public int IdCompra { get; set; }
+        public int IdUsuario { get; set; }
+        public DateTime FechaCompra { get; set; }
+        public int IdProveedor { get; set; }
+        public int IdComprobante { get; set; }
+        public double CostoCompra { get; set; }
+
+        public Compras()
+        {
+            IdCompra = 0;
+            IdUsuario = 0;
+            FechaCompra = DateTime.Now;
+            IdProveedor = 0;
+            IdComprobante = 0;
+            CostoCompra = 0;
+        }
+    }
+}
