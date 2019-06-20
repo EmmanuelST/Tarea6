f2cd32a74675a9c69081253bbe52333be4f4b817
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-19 20:50:11 -0400

    Agregando la entidad Personas

---
+++ Tarea6/Entidades/Personas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6.Entidades
+{
+    class Personas
+    {
+        [Key]
+        public int IdPersona { get; set; }
+        public int Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Matemo { get; set; }
+        public string Paterno { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefonon { get; set; }
+        public string Correo { get; set; }
+        public char Sexo { get; set; }
+        public string Direccion { get; set; } 
+
+    }
+}
